c8667c0f486b409a7cd5cce545704e99c9e60222
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-09-29 13:45:36 +0200

    Secrets

---
+++ secret-example.py
@@ -0,0 +1,25 @@
+#tokens from Reddit
+client_id=
+client_secret=
+password=
+
+#tokens from Telegram
+user_agent=
+username=
+token=
+
+#Data mine names
+Data1=
+data1csv=
+
+Data2=
+data2csv=
+
+Data3=
+data3csv=
+
+Data4=
+data4csv=
+
+Data5=
+data5csv=
